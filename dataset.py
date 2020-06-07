fa7781f7249bb5ede4f824bfa98388b5d3c99a1b
Ilan Olkies <ilanolkies@outlook.com>
2020-06-07 01:44:03 -0300

    Add comments

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# normalización de valores entre -1 y 1
 def normalize(data):
   for c in data.columns:
     M = data[c].max()
@@ -7,15 +8,19 @@ def normalize(data):
     data[c] = (data[c] - m) / (M - m)
     data[c] = data[c] * 2 - 1
 
+# carga de datos dadas las columnas usadas como entrada y target,
+# y una funcion aplicada al target
 def load_dataset(ds_file, input_break, target_break, apply_target):
   df = pd.read_csv(ds_file, header=None)
 
+  # datos de entrada
   data_input = pd.DataFrame(df[df.columns[
     input_break[0]:input_break[1]]
   ])
 
   normalize(data_input)
 
+  # target
   data_target = df[df.columns[
     target_break[0]:target_break[1]]
   ]
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from plotter import plot_error
 
 B = 1 # para cambiar buckets
 
+# funciones de inicialización
 def init_w(S):
   return [
     0 if i == 0 else
@@ -32,7 +33,7 @@ def init_y(S):
 
   return _Y
 
-# agregar y sacar umbrales.
+# agregar y sacar umbrales
 def bias_add(V):
   bias = -np.ones((len(V), 1))
   return np.concatenate((V, bias), axis=1)
@@ -40,6 +41,9 @@ def bias_add(V):
 def bias_sub(V):
   return V[:,:-1]
 
+# funciones de entrenamiento
+
+# activacion feed forward
 def activation(S, Xh, W):
   Y = init_y(S)
   _Y = Xh
@@ -48,19 +52,20 @@ def activation(S, Xh, W):
 
   for k in range(1, L):
     Y[k-1][:] = bias_add(_Y)
-    _Y = np.tanh(np.dot(Y[k-1], W[k])) # aca setea funcion g
+    _Y = np.tanh(np.dot(Y[k-1], W[k])) # función de activación g
 
   Y[L-1][:] = _Y
 
   return Y
 
+# correccion backpropagation
 def correction(S, Zh, W, Y, lr):
   L = len(S)
 
-  dW = init_dw(W) # Inicializo vector de matrices dW con dimensiones de W.
-  D = init_y(S) # Inicializo vector D con dimensiones de Y.
-  E = Zh - Y[L-1] # Error en la salida.
-  dY = 1 - np.square(Y[L-1]) # Derivada de la función de activación en la salida.
+  dW = init_dw(W)
+  D = init_y(S)
+  E = Zh - Y[L-1]
+  dY = 1 - np.square(Y[L-1]) # derivada de la función de activación
   D[L-1] = E*dY
 
   for k in range(L-1, 0, -1):
@@ -80,6 +85,7 @@ def estimation(Zh, Y):
   return np.mean(np.sum(np.square(Zh-Y[-1]), axis=1))
 
 def train(X, Z, S, max_epoch, lr):
+  # inicialización de matriz de pesos
   W = init_w(S)
 
   errors = []
@@ -87,27 +93,30 @@ def train(X, Z, S, max_epoch, lr):
   t = 0
 
   while(error > 0.001 and t < max_epoch):
+    # resultado parcial
     if t % 100 == 0:
       print('epoch {} - error {}/{}'.format(t, error, len(X)))
 
     error = 0
-    H = np.random.permutation(len(X)) # mini-lotes (recorro instancias en ordenes random)
+
+    # mini-lotes - mezcla de orden de instancias
+    H = np.random.permutation(len(X))
 
     for h in H:
       # TODO: if h+B <= P: - para B > 1
-      Y = activation(S, X[h:h + B], W) # pasamos una vez cada instancia
-      dW = correction(S, Z[h:h+B], W, Y, lr) # calculamos dW
-      W = adaptation(W, dW) # actualizamos W con dW
-      error += estimation(Z[h:h+B], Y) # suma de los errores de cada instancia
+      Y = activation(S, X[h:h + B], W)
+      dW = correction(S, Z[h:h+B], W, Y, lr)
+      W = adaptation(W, dW)
+      error += estimation(Z[h:h+B], Y)
 
     t += 1
-    errors.append(error) # errores x epoch
+    errors.append(error)
 
   return errors, W
 
 def test(X, Z, S, W):
   Y = [np.sign(activation(S, X[i:i+1], W)[-1][0]) for i in range(len(X))]
-  return np.mean(np.where(Y == Z, 1, 0))
+  return np.mean(np.where(Y == Z, 1, 0)) # promedio de aciertos
 
 def main(
   argv, input_break, target_break, apply_target,
@@ -118,6 +127,7 @@ def main(
 
   [_, model, data] = argv
 
+  # lectura y pre procesamiento de datos
   (input, target) = load_dataset(
     data,
     input_break,
@@ -126,11 +136,19 @@ def main(
   )
 
   if (not path.exists(model + '.p')):
+    # entrenamiento
     errors, W = train(input, target, S, max_epoch, lr)
+
     plot_error(errors)
     pickle.dump(errors, open(model + '_errors.p', 'wb'))
     pickle.dump(W, open(model + '.p', 'wb'))
   else:
+    # testing
+
+    # carga de modelo entrenado
     W = pickle.load(open(model + '.p', 'rb'))
+
+    # testeo
     r = test(input, target, S, W)
+
     print('precisión: {}'.format(r))
