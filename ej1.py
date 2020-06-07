014bfac7fb21d8cde5cfae7d3755b47c9f80ba51
Ilan Olkies <ilanolkies@outlook.com>
2020-06-06 22:14:20 -0300

    Detectar si existe modelo e inicializar train

--- ej1.py
+++ ej1.py
@@ -5,10 +5,13 @@ import numpy as np
 def apply_target(data_target):
   return np.where(data_target=='M', -1, 1)
 
+S = [10,9,1]
+
 main(
   (1,11),
   (0,1),
-  apply_target
+  apply_target,
+  S
 )
 
 # import main
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import sys
 from dataset import load_dataset
+from os import path
+import numpy as np
 
-def main(input_break, target_break, apply_target):
+def initialize(S):
+  L = len(S)
+
+  W = [0]
+  for i in range(L-1):
+    W.append(
+      np.random.normal(
+        0, S[0]**(-1/2), (S[i]+1, S[i+1])
+      )
+    )
+
+  dW = [0]
+  for i in range(L-1):
+    dW.append(np.zeros_like(W[i+1]))
+
+  return (W, dW)
+
+def train(input, target, S):
+  (W, dW) = initialize(S)
+  print(W)
+
+def main(input_break, target_break, apply_target, S):
   if (len(sys.argv) != 3):
     print('Error en la entrada')
 
@@ -13,3 +36,6 @@ def main(input_break, target_break, apply_target):
     target_break,
     apply_target
   )
+
+  if (not path.exists(model)):
+    train(input, target, S)
